df31bcf8c092e7486b7f6cf082327b166a674f44
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-10-02 14:18:08 -0300

    Agregando recursos necesarios al proyecto...

---
+++ Archivos Ejecutables/marioGame.js
@@ -0,0 +1,9 @@
+// VARIABLES A DECLARAR...
+
+var container = document.getElementById("container");
+var foreground = document.getElementById("foreground");
+var plants = document.getElementById("plantas");
+var mountains = document.getElementById("montanas");
+var clouds = document.getElementById("nubes");
+var mario = document.getElementById("mario");
+var items = document.getElementById("items");
