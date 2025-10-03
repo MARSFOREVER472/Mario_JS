d66bdd245551d8a65f7de37ba4ee48325cb1dab1
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-10-03 13:23:07 -0300

    Agregando más código al juego...

--- Archivos Ejecutables/marioGame.js
+++ Archivos Ejecutables/marioGame.js
@@ -12,3 +12,43 @@ var runningLeft = 0;
 var score = 0;
 var coins = 0;
 var dead = 0;
+
+// AJUSTANDO INTERVALO DE ANIMACIÓN A CADA ELEMENTO DEL JUEGO...
+
+// EL PERSONAJE SE MUEVE HACIA LA DERECHA...
+
+setInterval(function()
+{
+    if (runningRight == 1)
+    {
+        move(foreground, -40)
+        move(items, -40)
+        move(plants, -23)
+        move(mountains, -6)
+        move(clouds, -4)
+
+        mario.classList.remove("lookingLeft");
+        mario.classList.add("lookingRight");
+        mario.classList.add("runningRight");
+    }
+
+}, 10);
+
+// EL PERSONAJE SE MUEVE HACIA LA IZQUIERDA...
+
+setInterval(function()
+{
+    if (runningLeft == 1)
+    {
+        move(foreground, 40)
+        move(items, 40)
+        move(plants, 23)
+        move(mountains, 6)
+        move(clouds, 4)
+
+        mario.classList.remove("lookingRight");
+        mario.classList.add("lookingLeft");
+        mario.classList.add("runningLeft");
+    }
+
+}, 10);
